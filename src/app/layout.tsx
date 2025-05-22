3b2bca7f95e5e02a61722977dcdb3a312bc8b47b
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-05-22 15:21:56 -0400

    Homepage, header, and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,8 @@ export default function RootLayout({
             <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
             <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
           </div>
-          {children}
+          <main className="pt-24 flex-grow">{children}</main>
+
           <Footer />
         </body>
       </html>

ee1057627d8c02c5791fe2bb235a618af5d0ed7e
Syscily Brown <sbrown@syscilythedeveloper.com>
2025-05-30 00:51:47 -0400

    function for querying plans

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { api } from "../../../convex/_generated/api";
+import { useQuery } from "convex/react";
+import { useUser } from "@clerk/nextjs"; // Assuming Clerk is used for user management
 
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const { user } = useUser(); // Assuming useUser is a custom hook to get user data
+  const userId = user?.id ?? "";
+  const allPlans = useQuery(api.plans.getUserPlans, { userId });
+  const [selectedPlanId, setSelectedPlanId] = useState<string | null>(null);
+
+  const activePlan = allPlans?.find((plan) => plan.isActive);
+
+  const currentPlan = selectedPlanId
+    ? allPlans?.find((plan) => plan._id === selectedPlanId)
+    : activePlan;
+
+  return <section></section>;
 };
 
 export default ProfilePage;
